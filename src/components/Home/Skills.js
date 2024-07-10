1913b8c479b6e6416bc22f7abe9c9665bd3938b4
shoebabedin <shoeb.abedin@intelli.global>
2024-07-10 14:14:49 +0600

    update

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -1,6 +1,5 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import * as DevIcons from 'devicons-react';
+import React from "react";
 import data from "./../../assets/json/skills.json";
 
 const Skills = () => {
@@ -23,6 +22,7 @@ const Skills = () => {
           <div className="row">
             <div className="col-md-12 text-center fh5co-heading">
               <h2>Skills</h2>
+              
             </div>
           </div>
           {data?.map((item) => (
@@ -33,21 +33,25 @@ const Skills = () => {
                   <span></span>
                 </h1>
               </div>
-              {item.stackList.map((list) => (
-                <div key={list.id} className="col-md-3 col-6 text-center">
-                  {/* <img
-                    src={require(`./../../assets/images/stacks-logo/${list.img}`)}
-                    className="img-fluid"
-                    alt={list.img}
-                  /> */}
-                  <div className="chart" data-percent="100">
-                    <span>
-                      <strong>{list.title}</strong>
-                    </span>
-                    <canvas height="160" width="160"></canvas>
+              {item.stackList.map((list) => {
+                const IconComponent = DevIcons[list.iconComponent];
+                return (
+                  <div key={list.id} className="col-md-3 col-6 text-center" >
+                    <div className="border mb-3 py-3" style={{background: list.color}}>
+                      {IconComponent ? (
+                        <IconComponent size="80" />
+                      ) : (
+                        <div className="chart" data-percent="100">
+                          <span>
+                            <strong>{list.title}</strong>
+                          </span>
+                          <canvas height="160" width="160"></canvas>
+                        </div>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
           ))}
         </div>
